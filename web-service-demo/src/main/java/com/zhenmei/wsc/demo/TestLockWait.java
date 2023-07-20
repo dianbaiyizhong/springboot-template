ed970fa9ea3f70f07144d48e858d12d48fc7333f
huanghaoming <1585685035@qq.com>
2023-07-20 20:34:29 +0800

    新增druid

--- web-service-demo/src/main/java/com/zhenmei/wsc/demo/TestLockWait.java
+++ web-service-demo/src/main/java/com/zhenmei/wsc/demo/TestLockWait.java
@@ -1,10 +1,17 @@
 package com.zhenmei.wsc.demo;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.zhenmei.wsc.demo.service.LockWaitServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Component
 @EnableScheduling
@@ -13,11 +20,25 @@ public class TestLockWait {
     @Autowired
     private LockWaitServiceImpl lockWaitService;
 
-    @Scheduled(fixedRate = 200000)
-    public void execute() {
-        for (int i = 0; i < 200; i++) {
-            lockWaitService.executeSome();
+
+    @Autowired
+    private DruidDataSource druidDs;
+
+    @Scheduled(fixedRate = 10000)
+    public void execute() throws SQLException {
+//        for (int i = 0; i < 200; i++) {
+//            lockWaitService.executeSome();
+//        }
+        Connection conn = druidDs.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement("select * from t_user_role");
+        ResultSet resultSet = pstmt.executeQuery();
+
+        while (resultSet.next()) {
+            System.out.println("-------");
         }
+
+        DruidDs.password = null;
+        druidDs.setPassword(null);
     }
 
 
